20bb176fbd2bcb3a583679642b6fea96c7af4144
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-09-13 21:29:32 +0100

    Add logic to add book to the array list

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const list = [];
-const inputEl = document.querySelectorAll('input')
+const books = [];
 
+const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author');
+const bookContainer = document.querySelector('#ptext');
 
 const addBtn = document.getElementById('add-btn');
 
-addBtn.onclick = function () {
-};
-function addBook() {
-const title = document.getElementById("title").value;
-const author = document.getElementById("author").value; 
-  const arrobj = ({ title, author });
-  list.push(arrobj);
+function addBook(title, author) {
+  books.push({title, author});
+
+  return { title, author };
 }
+
+function createBook({ title, author }) {
+  const bookDiv = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+
+  bookTitle.textContent = title;
+  bookAuthor.textContent = author;
+
+  bookDiv.append(bookTitle, bookAuthor);
+  bookContainer.appendChild(bookDiv);
+}
+
+books.forEach(createBook);
+
+addBtn.onclick = function (e) {
+  e.preventDefault();
+ 
+  const newBook = addBook(
+    titleInput.value,
+    authorInput.value
+  );
+
+  createBook(newBook);
+  titleInput.value = '';
+  authorInput.value = '';
+};
+
