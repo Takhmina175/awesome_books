a9259b2969bb378c77c2e97a61c6caf5939df2e1
mina <takhminam@gmail.com>
2021-09-14 00:38:16 +0300

    add remove function

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
-const books = [];
+let books = [];
+if (localStorage.getItem("books") === null) {
+  books = [];
+} else {
+  books = JSON.parse(localStorage.getItem("books"));
+}
 
-const titleInput = document.querySelector('#title')
-const authorInput = document.querySelector('#author');
-const bookContainer = document.querySelector('#ptext');
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const bookContainer = document.querySelector("#ptext");
 
-const addBtn = document.getElementById('add-btn');
+const addBtn = document.getElementById("add-btn");
 
 function addBook(title, author) {
-  books.push({title, author});
+  books.push({ title, author });
+    localStorage.setItem("books", JSON.stringify(books));
 
   return { title, author };
 }
 
+function removeEl(index){
+   books = books.filter(function (_, value) {
+     return value !== index;
+   });
+}
+
 function createBook({ title, author }) {
-  const bookDiv = document.createElement('div');
-  const bookTitle = document.createElement('p');
-  const bookAuthor = document.createElement('p');
+  const bookDiv = document.createElement("div");
+  const bookTitle = document.createElement("p");
+  const bookAuthor = document.createElement("p");
+  const btnRemove = document.createElement("button")
 
   bookTitle.textContent = title;
   bookAuthor.textContent = author;
+  btnRemove.textContent = "Remove";
 
-  bookDiv.append(bookTitle, bookAuthor);
+  btnRemove.setAttribute("id", "btn-rm");
+  bookDiv.setAttribute("class", "delete")
+
+
+  bookDiv.append(bookTitle, bookAuthor, btnRemove);
   bookContainer.appendChild(bookDiv);
+
+  
 }
 
 books.forEach(createBook);
 
 addBtn.onclick = function (e) {
   e.preventDefault();
- 
-  const newBook = addBook(
-    titleInput.value,
-    authorInput.value
-  );
+
+  const newBook = addBook(titleInput.value, authorInput.value);
 
   createBook(newBook);
-  titleInput.value = '';
-  authorInput.value = '';
+  titleInput.value = "";
+  authorInput.value = "";
+};
+
+const btnRm = document.getElementById('btn-rm');
+btnRm.onclick = function (e) {
+
+let bookRemove = e.target.parentElement;
+bookContainer.removeChild(bookRemove);
 };
 
