d9e3328929623970a639eaeec5df4bf88eefd61d
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-09-14 15:12:07 +0100

    Try to write function to remove books but could not

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const bookContainer = document.querySelector('#ptext');
+const addBtn = document.getElementById('add-btn');
+
 let books = [];
-if (localStorage.getItem("books") === null) {
+if (localStorage.getItem('books') === null) {
   books = [];
 } else {
-  books = JSON.parse(localStorage.getItem("books"));
+  books = JSON.parse(localStorage.getItem('books'));
 }
 
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const bookContainer = document.querySelector("#ptext");
 
-const addBtn = document.getElementById("add-btn");
 
-function addBook(title, author) {
-  books.push({ title, author });
-    localStorage.setItem("books", JSON.stringify(books));
-
-  return { title, author };
-}
-
-function removeEl(index){
-   books = books.filter(function (_, value) {
-     return value !== index;
-   });
-}
 
 function createBook({ title, author }) {
-  const bookDiv = document.createElement("div");
-  const bookTitle = document.createElement("p");
-  const bookAuthor = document.createElement("p");
-  const btnRemove = document.createElement("button")
+  const bookDiv = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const btnRemove = document.createElement('button')
 
   bookTitle.textContent = title;
   bookAuthor.textContent = author;
-  btnRemove.textContent = "Remove";
+  btnRemove.textContent = 'Remove';
 
-  btnRemove.setAttribute("id", "btn-rm");
-  bookDiv.setAttribute("class", "delete")
+  btnRemove.setAttribute('id', 'btn-rm');
+  bookDiv.setAttribute('class', 'delete')
 
 
   bookDiv.append(bookTitle, bookAuthor, btnRemove);
@@ -46,20 +35,27 @@ function createBook({ title, author }) {
 
 books.forEach(createBook);
 
+function addBook(title, author) {
+  books.push({ title, author });
+  localStorage.setItem('books', JSON.stringify(books));
+
+  return { title, author };
+}
+
 addBtn.onclick = function (e) {
   e.preventDefault();
 
   const newBook = addBook(titleInput.value, authorInput.value);
 
   createBook(newBook);
-  titleInput.value = "";
-  authorInput.value = "";
+  titleInput.value = ''
+  authorInput.value = ''
 };
 
 const btnRm = document.getElementById('btn-rm');
-btnRm.onclick = function (e) {
-
-let bookRemove = e.target.parentElement;
-bookContainer.removeChild(bookRemove);
+btnRm.onclick = function (index) {
+    books.filter(function(book) {
+      console.log(book !== index);
+    })
 };
 
