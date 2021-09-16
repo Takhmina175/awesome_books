3c324028e94a4ff6e7995642b5fbe2811c95531f
mina <takhminam@gmail.com>
2021-09-16 20:00:09 +0300

    Add style to the input value

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ const ul = document.createElement('ul');
 
 function createBook({ title, author }) {
   const listItem = document.createElement('li');
+  const bookDiv = document.createElement('div');
+  const btnDiv = document.createElement('div');
   const paraTitle = document.createElement('p');
   const paraAuthor = document.createElement('p');
   const btnRemove = document.createElement('button');
-  paraTitle.textContent = title;
+
+  paraTitle.innerHTML = `${title} by`;
   paraAuthor.textContent = author;
   btnRemove.textContent = 'Remove';
   listItem.className = 'list-item';
   btnRemove.className = 'delete';
   btnRemove.setAttribute('id', 'btn-rm');
-  listItem.append(paraTitle, paraAuthor, btnRemove);
+  bookDiv.className = 'bookDiv';
+  btnDiv.className = 'btnDiv';
+  bookDiv.append(paraTitle, paraAuthor);
+  btnDiv.appendChild(btnRemove);
+  listItem.append(bookDiv, btnDiv);
   ul.appendChild(listItem);
   bookContainer.appendChild(ul);
 }
@@ -33,7 +40,7 @@ addBtn.onclick = (e) => {
 ul.addEventListener('click', (e) => {
   const classes = e.target.className;
   const classesArray = classes.split(' ');
-  const item2BeRemoved = e.target.parentElement;
+  const item2BeRemoved = e.target.parentElement.parentElement;
   const nodes = Array.from(ul.children);
   const index = nodes.indexOf(item2BeRemoved);
   if (classesArray.indexOf('delete') !== -1) {
